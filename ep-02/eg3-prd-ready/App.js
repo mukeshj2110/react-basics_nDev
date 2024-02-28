48585eadff7df376932f99dbd71773fae66728a9
mukeshj2110 <mukeshjadhav680@gmail.com>
2024-02-28 15:29:40 +0530

    ep-02production ready react app

---
+++ ep-02/eg3-prd-ready/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// div 
+//     div 
+//         h1
+//     div 
+// div
+
+
+const parent1  = React.createElement("div",{
+    id: "parent"
+} , React.createElement("div",{id: "child"}, React.createElement("h1",{} , "im h1 tag")));
+
+
+//If you want to add sibiling to h1 tag you need to create array
+// div 
+//     div 
+//         h1
+//         h1
+//     div 
+// div
+
+const parent  = React.createElement("div",{
+    id: "parent"
+} , React.createElement("div",{id: "child"}, 
+    [React.createElement("h1",{} , "im h1 tag"),React.createElement("h1",{} , "im h1 tag 2")]));
+
+const heading  = React.createElement("h1",{
+    id: "heading"
+} , "hello from react!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+console.log(heading);  //***************IMP */
+
+root.render(parent);
